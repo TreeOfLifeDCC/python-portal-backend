aabe539b741f31df754b0b8e1e3b3370fd2abdcc
Yogmatee Roochun <yroochun@ebi.ac.uk>
2024-10-02 17:21:32 +0100

    abc-359: added download functionality

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 import csv
-import os
 import re
 from io import StringIO
-from typing import Optional, List
+from typing import Optional
 from elasticsearch import AsyncElasticsearch, AIOHttpConnection
 from fastapi import FastAPI, Response
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import io
 
 import json
 
@@ -393,9 +395,9 @@ async def export_csv(
 
 @app.get("/{index}")
 async def root(index: str, offset: int = 0, limit: int = 15,
-               sort: str = "currentSt1ssaxn1atus:asc", filter: str = None,
+               sort: str = "currentStatus:asc", filter: str = None,
                search: str = None, current_class: str = 'kingdom',
-               phylogeny_filters: str = None):
+               phylogeny_filters: str = None, action: str = None):
     global value
     print(phylogeny_filters)
     # data structure for ES query
@@ -548,10 +550,12 @@ async def root(index: str, offset: int = 0, limit: int = 15,
                               {"value": f"*{search}*",
                                "case_insensitive": True}}}
         )
-    print(json.dumps(body))
-    response = await es.search(
-        index=index, sort=sort, from_=offset, size=limit, body=body
-    )
+
+    if action == 'download':
+        response = await es.search(index=index, sort=sort, from_=offset, body=body, size=50000)
+    else:
+        response = await es.search(index=index, sort=sort, from_=offset, size=limit, body=body)
+
     data = dict()
     data['count'] = response['hits']['total']['value']
     data['results'] = response['hits']['hits']
@@ -591,3 +595,112 @@ async def details(index: str, record_id: str):
     if index == 'data_portal':
         data['aggregations'] = aggregations
     return data
+
+
+class QueryParam(BaseModel):
+    pageIndex: int
+    pageSize: int
+    searchValue: str = ''
+    sortValue: str
+    filterValue: str = ''
+    currentClass: str
+    phylogenyFilters: str
+    indexName: str
+    downloadOption: str
+
+
+@app.post("/data-download")
+async def get_data_files(item: QueryParam):
+    data = await root(item.indexName, 0, item.pageSize,
+                      item.sortValue, item.filterValue,
+                      item.searchValue, item.currentClass,
+                      item.phylogenyFilters, 'download')
+
+    csv_data = create_data_files_csv(data['results'], item.downloadOption)
+
+    # Return the byte stream as a downloadable CSV file
+    return StreamingResponse(
+        csv_data,
+        media_type='text/csv',
+        headers={"Content-Disposition": "attachment; filename=download.csv"}
+    )
+
+
+def create_data_files_csv(results, download_option):
+    header = []
+    if download_option.lower() == "assemblies":
+        header = ["Scientific Name", "Accession", "Version", "Assembly Name", "Assembly Description",
+                  "Link to chromosomes, contigs and scaffolds all in one"]
+    elif download_option.lower() == "annotation":
+        header = ["Annotation GTF", "Annotation GFF3", "Proteins Fasta", "Transcripts Fasta",
+                  "Softmasked genomes Fasta"]
+    elif download_option.lower() == "raw_files":
+        header = ["Study Accession", "Sample Accession", "Experiment Accession", "Run Accession", "Tax Id",
+                  "Scientific Name", "FASTQ FTP", "Submitted FTP", "SRA FTP", "Library Construction Protocol"]
+    elif download_option.lower() == "metadata":
+        header = ['Organism', 'Common Name', 'Common Name Source', 'Current Status']
+
+    output = io.StringIO()
+    csv_writer = csv.writer(output)
+    csv_writer.writerow(header)
+
+    for entry in results:
+        record = entry["_source"]
+        if download_option.lower() == "assemblies":
+            assemblies = record.get("assemblies", [])
+            scientific_name = record.get("organism", "")
+            for assembly in assemblies:
+                accession = assembly.get("accession", "-")
+                version = assembly.get("version", "-")
+                assembly_name = assembly.get("assembly_name", "")
+                assembly_description = assembly.get("description", "")
+                link = f"https://www.ebi.ac.uk/ena/browser/api/fasta/{accession}?download=true&gzip=true" if accession else ""
+                entry = [scientific_name, accession, version, assembly_name, assembly_description, link]
+                csv_writer.writerow(entry)
+
+        elif download_option.lower() == "annotation":
+            annotations = record.get("annotation", [])
+            for annotation in annotations:
+                gtf = annotation.get("annotation", {}).get("GTF", "-")
+                gff3 = annotation.get("annotation", {}).get("GFF3", "-")
+                proteins_fasta = annotation.get("proteins", {}).get("FASTA", "")
+                transcripts_fasta = annotation.get("transcripts", {}).get("FASTA", "")
+                softmasked_genomes_fasta = annotation.get("softmasked_genome", {}).get("FASTA", "")
+                entry = [gtf, gff3, proteins_fasta, transcripts_fasta, softmasked_genomes_fasta]
+                csv_writer.writerow(entry)
+
+        elif download_option.lower() == "raw_files":
+            experiments = record.get("experiment", [])
+            for experiment in experiments:
+                study_accession = experiment.get("study_accession", "")
+                sample_accession = experiment.get("sample_accession", "")
+                experiment_accession = experiment.get("experiment_accession", "")
+                run_accession = experiment.get("run_accession", "")
+                tax_id = experiment.get("tax_id", "")
+                scientific_name = experiment.get("scientific_name", "")
+                submitted_ftp = experiment.get("submitted_ftp", "")
+                sra_ftp = experiment.get("sra-ftp", "")
+                library_construction_protocol = experiment.get("library_construction_protocol", "")
+                fastq_ftp = experiment.get("fastq_ftp", "")
+
+                if fastq_ftp:
+                    fastq_list = fastq_ftp.split(";")
+                    for fastq in fastq_list:
+                        entry = [study_accession, sample_accession, experiment_accession, run_accession, tax_id,
+                                 scientific_name, fastq, submitted_ftp, sra_ftp, library_construction_protocol]
+                        csv_writer.writerow(entry)
+                else:
+                    entry = [study_accession, sample_accession, experiment_accession, run_accession, tax_id,
+                             scientific_name, fastq_ftp, submitted_ftp, sra_ftp, library_construction_protocol]
+                    csv_writer.writerow(entry)
+
+        elif download_option.lower() == "metadata":
+            organism = record.get('organism', '')
+            common_name = record.get('commonName', '')
+            common_name_source = record.get('commonNameSource', '')
+            current_status = record.get('currentStatus', '')
+            entry = [organism, common_name, common_name_source, current_status]
+            csv_writer.writerow(entry)
+
+    output.seek(0)
+    return io.BytesIO(output.getvalue().encode('utf-8'))
