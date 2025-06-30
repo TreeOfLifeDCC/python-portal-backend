c4964045f5d44534fb469e7dda389ae9d9794e91
Raheelaaslam1024 <160017141+Raheelaaslam1024@users.noreply.github.com>
2025-06-30 10:03:21 +0100

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -2,7 +2,8 @@ DATA_PORTAL_AGGREGATIONS = [
     "biosamples", "raw_data", "mapped_reads", "assemblies_status",
     "annotation_status", "annotation_complete", "project_name",
     "symbionts_assemblies_status", "symbionts_biosamples_status", "symbionts_raw_data_status",
-    "metagenomes_assemblies_status","metagenomes_biosamples_status", "metagenomes_raw_data_status"
+    "metagenomes_assemblies_status","metagenomes_biosamples_status", "metagenomes_raw_data_status",
+    "images_available"
 ]
 
 ARTICLES_AGGREGATIONS = ["pubYear", "journalTitle", "articleType"]
